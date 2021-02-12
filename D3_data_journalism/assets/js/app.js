5ecdce3c0399b7f8e45d88019ed0c993a2aefd60
Paola Lopez <paola.lopez1@yahoo.com>
2021-02-12 04:06:56 -0800

    created function with tooltip to update circles' labels, initialized tooltip and created event listeners

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -26,7 +26,7 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Initial Params
-var chosenXAxis = "poverty_rate";
+var chosenXAxis = "poverty";
 var chosenYAxis = "healthcare"
 
 // function used for updating x-scale var upon click on axis label
@@ -79,8 +79,8 @@ function renderYAxes(newYScale, yAxis) {
 // function used for updating circles group with a transition to
 // new circles
 function renderCircles(circlesGroup, newXScale, chosenXAxis, newYScale, chosenYAxis) {
-
-  circlesGroup.transition()
+    
+    circlesGroup.transition()
     .duration(1000)
     .attr("cx", d => newXScale(d[chosenXAxis]))
     .attr("cy", d => newYScale(d[chosenYAxis]));
@@ -102,36 +102,60 @@ function renderText(textGroup, newXScale, chosenXAxis, newYScale, chosenYAxis) {
   }
 
 // function used for updating circles group with new tooltip
-function updateToolTip(chosenXAxis, circlesGroup) {
+function updateToolTip(chosenXAxis, chosenXAxis, circlesGroup, textGroup) {
+    
+    var label;
+    
+    if (chosenXAxis === "poverty") {
+        label = "In Poverty (%)";
+    }
+    else if (chosenXAxis === "age") {
+        label = "Age (Median)";
+    }
+    if (chosenYAxis === "healthcare") {
+        label = "In Poverty (%)";
+    }
+    else if (chosenYAxis === "smokes") {
+        label = "Smokes (%)";
+    }
 
-  var label;
-
-  if (chosenXAxis === "hair_length") {
-    label = "Hair Length:";
-  }
-  else {
-    label = "# of Albums:";
-  }
-
-  var toolTip = d3.tip()
-    .attr("class", "tooltip")
-    .offset([80, -60])
-    .html(function(d) {
-      return (`${d.rockband}<br>${label} ${d[chosenXAxis]}`);
+    // Initialize tooltip
+    var toolTip = d3.tip()
+        .attr("class", "tooltip")
+        .offset([80, -60])
+        .html(function(d) {
+            return (`<strong>${d.state}</strong><br>${xLabel} ${d[chosenXAxis]}<br>${yLabel} ${d[chosenYAxis]}`);
     });
 
-  circlesGroup.call(toolTip);
+// Create circles tooltip in chart and event listeners
+    circlesGroup.call(toolTip);
 
-  circlesGroup.on("mouseover", function(data) {
-    toolTip.show(data);
-  })
+    circlesGroup.on("mouseover", function(data) {
+        toolTip.show(data);
+    })
     // onmouseout event
-    .on("mouseout", function(data, index) {
-      toolTip.hide(data);
-    });
+        .on("mouseout", function(data, index) {
+            toolTip.hide(data);
+        });
+
+// Create text tooltip in chart and event listeners
+    circlesGroup.call(toolTip);
+
+    circlesGroup.on("mouseover", function(data) {
+        toolTip.show(data);
+    })
+    // onmouseout event
+        .on("mouseout", function(data, index) {
+            toolTip.hide(data);
+        });
 
   return circlesGroup;
 }
+
+
+
+
+
 
 // Retrieve data from the CSV file and execute everything below
 d3.csv("data.csv").then(function(peopleData, err) {
